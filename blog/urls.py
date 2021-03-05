448668b553ac9f92e8677469fa622dc84bd15f06
agata-bielas <bielas.agata@gmail.com>
2021-03-05 14:46:46 +0100

    Create comment model

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/new', views.post_new, name='post_new'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
